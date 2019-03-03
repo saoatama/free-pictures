8a762b19fc0d9aa044138001cf9097bae8250b34
saoatama <riotgarnet21@gmail.com>
2019-03-03 17:51:04 +0000

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get "index" => "index#index"
+  root to: 'index#index'
   get "index/:id" => "index#show"
   post "download" => "index#download"
   get "download" => "index#download"
