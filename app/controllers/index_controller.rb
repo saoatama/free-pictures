6a3df980ec6f3e66a3904a2b5d38e84029bd30ad
saoatama <riotgarnet21@gmail.com>
2019-03-03 14:35:35 +0000

    fixed 利用規約

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,11 @@
 class IndexController < ApplicationController
     layout 'index'
     def index
-        
+        @genres = Genre.all
     end
     
     def show
+        @genres = Genre.all
         @genre = Genre.find(params[:id])
     end
 end
