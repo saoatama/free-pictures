8ff405eb90d368a9291e29306fa71d64104533be
saoatama <riotgarnet21@gmail.com>
2019-03-04 23:54:23 +0000

    add devise

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,6 @@
 class Admin::GenresController < Admin::ApplicationController
   before_action :set_genre, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
     layout 'admin'
 
   # GET /genres
